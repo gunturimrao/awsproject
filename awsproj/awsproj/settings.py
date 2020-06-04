caf7d9c07966aa586ef9387a22b2d89453ee92e2
@maheshchowdary437 <maheshchowdary437@gmail.com>
2020-06-04 13:02:45 -0400

    safe code

--- awsproj/awsproj/settings.py
+++ awsproj/awsproj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e-22!8mir9cw_x49@vvc^e)#sb6lx&!ha(guqvd$(y7&o+4s1f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp'
+    'mainapp',
+    'login'
 ]
 
 MIDDLEWARE = [
@@ -74,12 +75,26 @@ WSGI_APPLICATION = 'awsproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+#
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
--- awsproj/mainapp/views.py
+++ awsproj/mainapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+ # Create your views here.
+# Note: The stuff above this will already be in this file.
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world!")
