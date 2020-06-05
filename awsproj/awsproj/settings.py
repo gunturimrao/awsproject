b6ed63beb3295156ee1b91a2cc79b54d436a6f48
Maheswara Rao Gunturi <gunturimrao@gmail.com>
2020-06-05 02:35:23 -0400

    ec2 code

--- awsproj/awsproj/settings.py
+++ awsproj/awsproj/settings.py
@@ -88,8 +88,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
